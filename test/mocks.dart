637a711754ae7cbbc11c9180fa6c4dbcdb2ac03f
Pavel J <pavelgj@gmail.com>
2013-05-08 09:00:58 -0700

    ugly hack for making tests run in dart2js

--- test/mocks.dart
+++ test/mocks.dart
@@ -13,8 +13,22 @@ class MockWindow extends Mock implements Window {
   MockDocument document = new MockDocument();
 }
 
-class MockHistory extends Mock implements History {}
+class MockHistory extends Mock implements History {
+  //TODO(pavelgj): ugly hack for making tests run in dart2js
+  void pushState(Object data, String title, [String url]) {
+    log.add(new LogEntry(name, 'pushState', [data, title, url], Action.IGNORE));
+  }
+
+  //TODO(pavelgj): ugly hack for making tests run in dart2js
+  void replaceState(Object data, String title, [String url]) {
+    log.add(new LogEntry(name, 'replaceState', [data, title, url], Action.IGNORE));
+  }
+}
 
 class MockLocation extends Mock implements Location {}
 
-class MockDocument extends Mock implements HtmlDocument {}
+class MockDocument extends Mock implements HtmlDocument {
+  //TODO(pavelgj): ugly hack for making tests run in dart2js
+  String set title(title) =>
+      log.add(new LogEntry(name, '=title', [title], Action.IGNORE));
+}
