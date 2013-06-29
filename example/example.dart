73b98877080b6c322f23652c8ec9a8878ff0ecd3
Justin Fagnani-Bell <justinfagnani@justinfagnani-macbookpro2.local>
2013-06-28 23:53:25 -0700

    Fix example

--- example/example.dart
+++ example/example.dart
@@ -2,17 +2,23 @@ library example;
 
 import 'dart:html';
 import 'package:route/client.dart';
+import 'package:logging/logging.dart';
 
 final one = new UrlPattern('/one');
 final two = new UrlPattern('/two');
+final example = new UrlPattern('/(.*)/example.html');
 
 main() {
+  Logger.root.level = Level.FINEST;
+  Logger.root.onRecord.listen((LogRecord r) { print(r.message); });
+
   query('#warning').remove();
   query('#one').classes.add('selected');
-  
+
   var router = new Router()
     ..addHandler(one, showOne)
     ..addHandler(two, showTwo)
+    ..addHandler(example, (_) => null)
     ..listen();
 }
 
