7a60ec9a493df3012daec6cc5892124642dc4879
Tim Blasi <tjblasi@google.com>
2014-07-30 11:24:21 -0700

    Use mock/mock.dart instead of deprecated unittest/mock.dart.

--- test/server_test.dart
+++ test/server_test.dart
@@ -2,12 +2,12 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
-
-import 'package:unittest/unittest.dart';
-import 'package:unittest/mock.dart';
-import 'package:route/server.dart';
 import 'dart:async';
 import 'dart:io';
+
+import 'package:mock/mock.dart';
+import 'package:route/server.dart';
+import 'package:unittest/unittest.dart';
 
 class HttpRequestMock extends Mock implements HttpRequest {
   Uri uri;
